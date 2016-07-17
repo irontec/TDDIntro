5759efa643fbaa187ee5695757eb19abdb5f49e0
kpicaza <kpicaza.sound@gmail.com>
2016-07-17 20:06:04 +0200

    Step 2 - Setting up PHPUnit

---
+++ tests/InitialPageTest.php
@@ -0,0 +1,13 @@
+<?php
+// tests/InitialPageTest.php
+namespace Tests\TDDIntro;
+
+class InitialPageTest extends \PHPUnit_Framework_TestCase
+{
+    public function testInitialList()
+    {
+        $this->assertTrue(true);
+    }
+
+}
+
